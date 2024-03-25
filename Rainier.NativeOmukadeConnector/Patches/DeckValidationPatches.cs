b49f01dd18337913651883636d5240934589defe
Zhong Lufan <lufanzhong@gmail.com>
2024-03-25 20:07:39 +0800

    ForceAllLegalityChecksToSucceed option allow any rule to use any card

--- Rainier.NativeOmukadeConnector/Patches/DeckValidationPatches.cs
+++ Rainier.NativeOmukadeConnector/Patches/DeckValidationPatches.cs
@@ -23,11 +23,35 @@ using SharedSDKUtils.DeckValidation;
 using SharedSDKUtils;
 using System;
 using System.Collections.Generic;
-using System.Text;
+using System.Linq;
+using TPCI.DeckValidation;
 using static SharedSDKUtils.DeckValidation.DeckValidationService;
 
 namespace Rainier.NativeOmukadeConnector.Patches
 {
+    [HarmonyPatch(typeof(DefaultDeckValidationController))]
+    static class DeckValidationAllUseExpandedFormat
+    {
+        [HarmonyPrepare]
+        static bool Prepare() => Plugin.Settings.ForceAllLegalityChecksToSucceed;
+
+        [HarmonyPatch(typeof(DefaultDeckValidationController), nameof(DefaultDeckValidationController.IsCardValidForFormat))]
+        [HarmonyPrefix]
+
+        static void IsCardValidForFormat_Prefix(ref string format)
+        {
+            format = "expanded";
+        }
+
+        [HarmonyPatch(typeof(DefaultDeckValidationController), nameof(DefaultDeckValidationController.ValidateDeck))]
+        [HarmonyPrefix]
+
+        static void ValidateDeck_Prefix(ref string format)
+        {
+            format = "expanded";
+        }
+    }
+
     [HarmonyPatch(typeof(DeckValidationService), nameof(DeckValidationService.ValidateDeck), typeof(RulesFormat), typeof(Dictionary<string, int>), typeof(DeckInfo), typeof(IQueryableCardDatabase), typeof(IConfigLoader))]
     static class DeckValidationAlwaysIgnoreUnowned
     {
@@ -46,42 +70,28 @@ namespace Rainier.NativeOmukadeConnector.Patches
 
         static void Postfix(ref DeckValidPackage __result)
         {
-            if (Plugin.Settings.ForceAllLegalityChecksToSucceed)
+            __result.entries = __result.entries.Where((state) =>
             {
-                __result.entries = __result.entries.Where((state) => state.error != DeckValidState.Banned || ClientPatches.ImplementedExpandedCardsFromServer.Contains(state.cardID)).ToArray();
-            }
+                return state.error != DeckValidState.Banned || ClientPatches.ImplementedExpandedCardsFromServer?.Contains(state.cardID) == true;
+
+            }).ToArray();
         }
     }
 
     [HarmonyPatch(typeof(RulesFormat), nameof(RulesFormat.IsCardValidForFormat))]
     static class RulesFormat_UseImplementedExpandedListInsteadOfFormats
     {
-        static bool Prepare() => Plugin.Settings.AskServerForImplementedCards;
+        static bool Prepare() => Plugin.Settings.AskServerForImplementedCards && Plugin.Settings.AskServerForImplementedCards;
 
         static bool Prefix(CardDataRow card, ref bool __result, DeckFormat ___format)
         {
-            // Only consider Expanded for this patch; don't perform this validation for other formats, or they will appear legal.
-            // This implementation makes all possible cards that aren't banned legal.
-            if( ___format != DeckFormat.EXPANDED)
-            {
-                return true;
-            }
-
-            // Don't process if no Implemented Cards data is available; warn about this instead.
-            if(ClientPatches.ImplementedExpandedCardsFromServer == null)
+            if (ClientPatches.ImplementedExpandedCardsFromServer == null)
             {
                 Plugin.SharedLogger.LogWarning("[RulesFormat] Ask Server For Implemented Cards is enabled, but no data available when checking format.");
-                return true;
             }
 
-            // If card is implemented serverside, always declare it valid for Expanded. It could still be banned (eg. Shaymin EX, Lysandre's Trump Card)
-            if(ClientPatches.ImplementedExpandedCardsFromServer.Contains(card.CardID))
-            {
-                __result = true;
-                return false;
-            }
-
-            return true;
+            __result = ClientPatches.ImplementedExpandedCardsFromServer?.Contains(card.CardID) == true;
+            return !__result;
         }
     }
 }
